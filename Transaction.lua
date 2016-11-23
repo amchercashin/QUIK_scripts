218e30b4a7aba96a8f0ee690346dd7e43363e537
amchercashin <amchercashin@gmail.com>
2016-11-23 13:45:54 +0300

    initial commit

---
+++ Transaction.lua
@@ -0,0 +1,27 @@
+t = {
+
+            ["CLASSCODE"]="TQBR",
+
+            ["SECCODE"]="LKOH",
+
+            ["ACTION"]="NEW_ORDER",
+
+            ["ACCOUNT"]="L01-00000F00",
+
+            ["CLIENT_CODE"]="40R9B",
+
+            ["TYPE"]="L",
+
+            ["OPERATION"]="B",
+
+            ["QUANTITY"]="1",
+
+            ["PRICE"]="100",
+
+            ["TRANS_ID"]="1"
+
+      }
+
+res=sendTransaction(t)
+
+message(res,1)
